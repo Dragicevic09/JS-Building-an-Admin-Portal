96021c435255fb76be3e60905038777cdbf42a07
Dragicevic09 <Sasadragicevic1995@gmail.com>
2023-07-21 18:21:41 -0700

    update js admin

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,18 @@
+async function updateBookTitle(id, newTitle) {
+    try {
+        const response = await fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ id, title: newTitle })
+        });
 
-// Your Code Here
+        const updatedBook = await response.json();
+        console.log(The Legends of Arathrae.title); // Display the updated book's title
+    } catch (error) {
+        console.error('Error updating book title:', error.message);
+    }
+}
+
+updateBookTitle(3, 'The Legends of Arathrae');
